b417191a81680ba33e6f2cfe7fc445749d18f5d2
Park Kyeul <gyul611@likelion.org>
2023-04-18 23:34:51 +0900

    today auto commit

---
+++ 1436.py
@@ -0,0 +1,12 @@
+# 아래 코드는 ChatGPT가 자동으로 생성한 코드입니다.
+# 문제 정보: 1436
+n = int(input())
+cnt = 0
+num = 666
+while True:
+    if '666' in str(num):
+        cnt += 1
+    if cnt == n:
+        print(num)
+        break
+    num += 1
